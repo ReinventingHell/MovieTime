fbba2587f1e3bc9c36fb98c6de122c563bf3bc05
“Bruno <shreddingmyheartout@gmail.com>
2012-04-25 06:04:36 -0300

    Initial Commit

---
+++ MovieTime/MTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MTAppDelegate.h
+//  MovieTime
+//
+//  Created by Bruno Tagliani on 4/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MovieTime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MovieTime
+//
+//  Created by Bruno Tagliani on 4/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MTAppDelegate class]));
+    }
+}
