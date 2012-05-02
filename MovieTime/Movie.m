f074f6d744899861182508c39fb8a7970d3354fb
“Bruno <shreddingmyheartout@gmail.com>
2012-05-02 01:21:13 -0300

    "Rellene" la clase Movie. Encontre la URL correcta para parsear las pelis! /web/peliculas?todas&page=X

--- MovieTime/Movie.m
+++ MovieTime/Movie.m
@@ -10,4 +10,45 @@
 
 @implementation Movie
 
+//Sintetizo los atributos
+@synthesize title = _title;
+@synthesize url = _url;
+@synthesize description = _description;
+@synthesize cast = _cast;
+@synthesize director = _director;
+@synthesize genre = _genre;
+@synthesize lang = _lang;
+@synthesize duration = _duration;
+@synthesize year =__year;
+
+//Metodos
+- (id)initWithTitle:(NSString *)pTitle
+                url:(NSString *)pUrl 
+        description:(NSString *)pDescription 
+               cast:(NSString *)pCast 
+           director:(NSString *)pDirector 
+              genre:(NSString *)pGenre 
+               lang:(NSString *)pLang
+           duration:(int)pDuration 
+               year:(int)pYear
+{
+    // Call the superclass's designated initializer
+    self = [super init];
+    // Give the instance variables initial values
+    if (self) {
+        [self setTitle:pTitle];
+        [self setUrl:pUrl];
+        [self setDescription:pDescription];
+        [self setCast:pCast];
+        [self setDirector:pDirector];
+        [self setGenre:pGenre];
+        [self setLang:pLang];
+        [self setDuration:pDuration];
+        [self setYear:pYear];
+    }
+    //    dateCreated = [[NSDate alloc] init];
+    // Return the address of the newly initialized object
+    return self;
+}
+
 @end
